ef11909e1b4b11ae2db2f308298ea2997c7dbeb0
javi <jaanga@live.com>
2016-05-12 22:15:15 +0200

    funcionalidad añadir pintores a la base de datos

--- admin/pintor/index.php
+++ admin/pintor/index.php
@@ -1,3 +1,55 @@
 <?php
 
+require_once '../app/info.php';
+require_once '../app/datadb.php';
+require_once '../db/connectdb.php';
+
+$base_path = $_SERVER['DOCUMENT_ROOT'].'/pinacotek/admin/';
+$base_imagen_path = $base_path . "imagenes/";
+
+
+if( isset($_GET['nuevopintor']) ){
+
+    $pais = htmlspecialchars($_POST['pais'], ENT_QUOTES, 'UTF-8');
+    $nombre = htmlspecialchars($_POST['nombre'], ENT_QUOTES, 'UTF-8');
+    $ciudad = htmlspecialchars($_POST['ciudad'], ENT_QUOTES, 'UTF-8');
+    $muerte = htmlspecialchars($_POST['muerte'], ENT_QUOTES, 'UTF-8');
+    $apellido = htmlspecialchars($_POST['apellido'], ENT_QUOTES, 'UTF-8');
+    $nacimiento = htmlspecialchars($_POST['nacimiento'], ENT_QUOTES, 'UTF-8');
+    $descripcion = htmlspecialchars($_POST['descripcion'], ENT_QUOTES, 'UTF-8');
+
+    $file_name = $_FILES['imagen']['name'];
+    $file_size = $_FILES['imagen']['size'];
+    $file_tmp = $_FILES['imagen']['tmp_name'];
+    $file_type = $_FILES['imagen']['type'];
+    $file_error = $_FILES['imagen']['error'];
+
+
+    try {
+          $sql = "INSERT INTO pintores (ciudad , foto , nombre , pais , muerte , nacimiento , descripcion , apellido) VALUES (:ciudad , :foto , :nombre , :pais , :muerte , :nacimiento , :descripcion , :apellido)";
+
+                  $foto = $base_path.'imagenes/'.$file_name;
+                  $ps = $pdo->prepare($sql);
+                  $ps->bindValue(':nombre', $nombre);
+                  $ps->bindValue(':apellido', $apellido);
+                  $ps->bindValue(':ciudad', $ciudad);
+                  $ps->bindValue(':pais', $pais);
+                  $ps->bindValue(':descripcion', $descripcion);
+                  $ps->bindValue(':muerte', $muerte);
+                  $ps->bindValue(':nacimiento', $nacimiento);
+                  $ps->bindValue(':foto', $file_name);
+                  $ps->execute();
+
+    } catch (Exception $e) {
+
+      die ("no se ha podido enviar la imagen");
+    }
+
+    move_uploaded_file($file_tmp, $base_imagen_path . $file_name);
+
+}else{
+
+  require_once 'pintor.html.php';
+}
+
 require_once 'pintor.html.php';
--- admin/pintor/pintor.html.php
+++ admin/pintor/pintor.html.php
@@ -4,58 +4,72 @@ require_once 'templates/header.html.php';
 
  ?>
 
-    <div class="row">
-        <div class="col-lg-12">
-          <div class="col-lg-5">
-            <h2>Nuevo pintor</h2>
-            <div class="form-group <?=$error_email?>">
-              <label>Nombre</label>
-                <input type="text" class="form-control" name="nombre">
-              <label>Apellido</label>
-                <input type="text" class="form-control" name="apellido">
-              <label>F.nacimineto</label>
-                <input type="text" class="form-control" name="nacimineto">
-              <label>Pais</label>
-                <input type="text" class="form-control" name="pais">
-              <label>Ciudad</label>
-                <input type="text" class="form-control" name="ciudad">
-              <label>Descripcion</label>
-                <textarea name="name" class="form-control" rows="4" cols="40" name="descripcion"></textarea>
-              <label>Foto pintor</label>
-                <input type="file" class="form-control btn-file" name="foto">
-              <button type="submit" class="btn btn-primary">Añadir</button>
-              </div>
-            </div>
-
-            <div class="col-lg-7">
-              <h2>Registro de pintores</h2>
-              <div class="lista table-responsive">
-                <table class="table table-bordered table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>apellido</th>
-                            <th>F.nacimineto</th>
-                            <th>pais</th>
-                            <th>ciudad</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <tr>
-                            <td>*****</td>
-                            <td>*****</td>
-                            <td>*****</td>
-                            <td>*****</td>
-                            <th>*****</th>
-                        </tr>
-                    </tbody>
-                  </table>
-            </div>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="col-lg-5">
+    <h2>Nuevo pintor</h2>
+     <form class="form-group" action="?nuevopintor" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label>Nombre</label>
+            <input type="text" class="form-control" name="nombre">
           </div>
-        </div>
-    </div>
-  </div>
+          <div class="form-group">
+            <label>Apellido</label>
+            <input type="text" class="form-control" name="apellido">
+          </div>
+          <div class="form-group">
+            <label>F.nacimineto</label>
+            <input type="text" class="form-control" name="nacimiento">
+          </div>
+          <div class="form-group">
+            <label>muerte</label>
+            <input type="text" class="form-control" name="muerte">
+          </div>
+          <div class="form-group">
+            <label>Pais</label>
+            <input type="text" class="form-control" name="pais">
+          </div>
+          <div class="form-group">
+            <label>Ciudad</label>
+            <input type="text" class="form-control" name="ciudad">
+          </div>
+          <div class="form-group">
+            <label>Descripcion</label>
+            <textarea class="form-control" rows="4" cols="40" name="descripcion"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="imagen">imagen</label>
+            <input type="file" name="imagen" id="imagen" value="">
+          </div>
+          <button type="submit" class="btn btn-primary">Añadir</button>
+        </form>
+      </div><!--FIN DIV COL-LG-5 -->
+      <div class="col-lg-7">
+        <h2>Registro de pintores</h2>
+          <div class="lista table-responsive">
+            <table class="table table-bordered table-hover table-striped">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>apellido</th>
+                  <th>F.nacimineto</th>
+                  <th>pais</th>
+                  <th>ciudad</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>*****</td>
+                  <td>*****</td>
+                  <td>*****</td>
+                  <td>*****</td>
+                  <th>*****</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div><!--FIN DIV COL-LG-7 -->
+      </div><!--FIN DIV COL-LG-12 -->
+    </div><!--fin div row -->
 </body>
 </html>
