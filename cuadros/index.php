62f72515cc3feab8528fa1facc3b85627a92dcb7
javi <jaanga@live.com>
2016-05-26 21:11:12 +0200

    funcionalidad añadir cuadros a favortito

--- cuadros/index.php
+++ cuadros/index.php
@@ -6,6 +6,34 @@ require_once '../db/cuadros.php';
 
 session_start();
 
+// Se añade cuadro a favorito
+if( isset($_GET['favorito']) ){
+
+	$id_cuadro = htmlspecialchars($_POST['id_cuadro'], ENT_QUOTES, 'UTF-8');
+	$id_usuario = htmlspecialchars($_POST['id_usuario'], ENT_QUOTES, 'UTF-8');
+
+	// var_dump($_POST);
+
+	try {
+
+		$sql = "INSERT INTO favoritos (id_cuadro , id_usuario) VALUES (:id_cuadro , :id_usuario)";
+		$ps = $pdo->prepare($sql);
+		$ps->bindValue(':id_cuadro', $id_cuadro);
+		$ps->bindValue(':id_usuario', $id_usuario);
+		$ps->execute();
+
+
+	} catch (Exception $e) {
+
+		  die ("no se ha podido añadir el cuadro a tus favoritos");
+
+	}
+
+	$cuadro = cuadro($id_cuadro);
+	require_once 'cuadro.html.php';
+	exit();
+}
+
 // Recibe el parametro enviado de pintor.php.html
 if ( isset($_POST['infocuadro']) ) {
 
@@ -20,6 +48,7 @@ if ( isset($_POST['infocuadro']) ) {
 
 }
 
+// Se cierra la sesion
 if( isset($_GET['logout']) ){
 
     // Se destruye la variable de sesión
