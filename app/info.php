625a069a519984a88b78ddaedcd3392bedc80d4a
javi <jaanga@live.com>
2016-05-29 18:38:27 +0200

    creada vista modificacion perfil de usuario

--- app/info.php
+++ app/info.php
@@ -4,3 +4,6 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 
 $base_url = "http://localhost:8080/pinacotek";
 $base_path = $_SERVER['DOCUMENT_ROOT'].'/pinacotek/';
+
+$admin_url = 'http://'.$_SERVER['HTTP_HOST'].'/pinacotek/';
+$base_urli = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
--- usuario/usuario.html.php
+++ usuario/usuario.html.php
@@ -2,22 +2,24 @@
 
     if( isset($_SESSION['user']) ){
           $user = $_SESSION['user'];
+          $id_user = $_SESSION['id'];
     }else{
 
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Pinacotek | Home</title>
+  <title>Pinacotek | Admin</title>
   <link rel="stylesheet" type="text/css" href="css/resert.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.css">
+  <!-- Link alternativo al de arriba por si no carga  -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="http://localhost:8080/pinacotek/css/header.css">
-  <link rel="stylesheet" type="text/css" href="http://localhost:8080/pinacotek/css/pintores.css">
+  <link rel="stylesheet" type="text/css" href="http://localhost:8080/pinacotek/css/cuadros.css">
   <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -33,15 +35,15 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?=$admin_url?>">Pinacotek</a>
+            <a class="navbar-brand" href="http://localhost:8080/pinacotek">Pinacotek</a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right col-lg-7">
-              <li class="active"><a href="<?=$admin_url?>">Pintores <span class="sr-only">(current)</span></a></li>
-              <li><a href="<?=$admin_url?>cuadros">Cuadros</a></li>
+              <li class="active"><a href="http://localhost:8080/pinacotek/">Pintores <span class="sr-only">(current)</span></a></li>
+              <li><a href="http://localhost:8080/pinacotek/cuadros">Cuadros</a></li>
               <?php  if( isset($_SESSION['user']) ) : ?>
-                <li><a href="<?=$admin_url?>favoritos">Colección</a></li>
+                <li><a href="http://localhost:8080/pinacotek/favoritos">Colección</a></li>
               <?php endif; ?>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Museos <span class="caret"></span></a>
@@ -58,14 +60,16 @@
               <li class="dropdown">
                 <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$user?><span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                  <li><a href="<?=$admin_url?>usuario">Configuracion</a></li>
+                  <li><a href="#">Configuracion</a></li>
+                  <li><a href="#">Ajustes</a></li>
+                  <li><a href="#">Acerca</a></li>
                   <li role="separator" class="divider"></li>
                   <li><a href="?logout">Cerrar Session</a></li>
                 </ul>
               </li>
               <?php endif; ?>
               <?php  if( !isset($_SESSION['user']) ) : ?>
-                  <li><a href="<?=$admin_url?>login">Login</a></li>
+                  <li><a href="http://localhost:8080/pinacotek/login">Login</a></li>
               <?php endif; ?>
               <div class="input-group busqueda col-lg-3">
                 <input type="text" class="form-control" placeholder="pintores , cuadros ...">
@@ -79,39 +83,6 @@
       </nav>
     </div><!-- fin row -->
 
-   <div class="row">
-     <div class="col-lg-offset-1 col-lg-10 resultados-widgets">
-       <div class="col-lg-3">
-         <h4>23 Pintores</h4>
-       </div>
-       <div class="col-lg-7">
-          <h4>orden</h4>
-          <form action="index.html" method="post">
-
-          </form>
-       </div>
-     </div>
-   </div>
-
-   <div class="row">
-     <div class="col-lg-offset-1 col-lg-10">
-       <?php foreach($pintores as $pi): ?>
-       <div class="col-sm-5 col-md-3 galeria">
-         <div class="thumbnail">
-           <img src="<?=$pi['foto']?>" alt="...">
-           <div class="caption">
-             <form id="pintor" action="?pintor" method="post">
-               <input type="hidden" name="id" value="<?=$pi['id']?>">
-               <?php echo  $pi['id']?>
-               <!-- <a href="#" onclick="document.getElementById('pintor').submit()"><?=$pi['nombre']?></a> -->
-               <button type="submit" name="enviar"><?=$pi['nombre']?></button>
-             </form>
-           </div>
-         </div>
-       </div>
-      <?php endforeach; ?>
-     </div>
-   </div>
 
  </div><!-- fin container -->
 </body>
