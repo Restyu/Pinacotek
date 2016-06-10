d0509acc3fddf23944e799fbc08593492002c817
javi <jaanga@live.com>
2016-06-10 20:07:37 +0200

    modificacion mensaje de registro

--- mail_sent.html.php
+++ mail_sent.html.php
@@ -33,11 +33,11 @@ $mail->AddReplyTo('pinacotek.info@gmail.com','pinacotek');
 //Y, ahora sí, definimos el destinatario (dirección y, opcionalmente, nombre)
 $mail->AddAddress( $email, $nombre);
 //Definimos el tema del email
-$mail->Subject = "prueba";
+$mail->Subject = "Registro pinacotek";
 //Para enviar un correo formateado en HTML lo cargamos con la siguiente función. Si no, puedes meterle directamente una cadena de texto.
 // $mail->MsgHTML(file_get_contents('correomaquetado.html'), dirname(ruta_al_archivo));
 //Y por si nos bloquean el contenido HTML (algunos correos lo hacen por seguridad) una versión alternativa en texto plano (también será válida para lectores de pantalla)
-$mail->Body = "lorem lorem";
+$mail->Body = "Gracias por registrase en pinacotek";
 
 // si el correo no se envia se imprime el error.
 if(!$mail->Send() ){
